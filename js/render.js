f2d7a025a87c661d0a761356cc796539e338662d
Jesús Rivera Flores <jesus982009@live.com>
2020-11-19 16:05:28 -0600

    arreglar y agregar animación

--- js/render.js
+++ js/render.js
@@ -1,48 +1,111 @@
+//import * as THREE from 'three';
+
+/********************* variables ******************************/
 //Tamaño de pantalla
 WIDTH = window.innerWidth; // Ancho de pantalla
 HEIGHT = window.innerHeight; // Alto de pantalla
 
-//Render dentro de <div>
-Render = new THREE.WebGLRenderer();
-//Render.setSize(800, 600);
-Render.setSize(WIDTH, HEIGHT);
-document.getElementById('render').appendChild(Render.domElement);
+//Preparamos el render
+var Render = new THREE.WebGLRenderer();
 
-//Escenario
-Escenario = new THREE.Scene();
+//El escenario
+var Escenario = new THREE.Scene();
 
-//Cámara
-Camara = new THREE.PerspectiveCamera();
-Camara.position.z=100;
-Escenario.add(Camara)
+//La cámara
+var Camara = new THREE.PerspectiveCamera();
 
-//Geometría para un punto rojo
-Geometria = new THREE.Geometry()
-ElVector = new THREE.Vector3(10,0,0);
-Geometria.vertices.push(ElVector);
+//La figura
+var Figura;
 
-//Geometría para una figura
-GeometriaLinea = new THREE.Geometry()
-var vertices = [[2,7,0], [7,2,0], [12,7,0], [12,17,0], [7,12,0], [2,17,0], [2,7,0]];
-var long_vertices = vertices.length;
-//Se analiza cada vector
-for(i = 0; i < long_vertices; i++){
-    x = vertices[i][0];
-    y = vertices[i][1];
-    z = vertices[i][2];
-    //SE agrega el vector a la geometría
-    Vector = new THREE.Vector3(x,y,z);
-    GeometriaLinea.vertices.push(Vector);
+//El vector
+var ElVector
+
+//Cotroles
+//var controls
+
+/******************************** funciones **************************************/
+inicio();
+animacion();
+/********************* inicio *******************************************/
+function inicio() {
+
+    //Tamaño del render
+    //Render.setSize(800, 600);
+    Render.setSize(WIDTH, HEIGHT);
+    //Agregar el render al html
+    document.getElementById('render').appendChild(Render.domElement);
+
+
+
+
+    //Acercar la camara a la posición en z
+    Camara.position.z = 100;
+    //Agregar la camara al escenario
+    Escenario.add(Camara)
+
+    //Cargar nuevos modelos
+    cargar_modelo();
+
+    //Agregar el escenario y la cámara al render
+    //Render.render(Escenario, Camara)
+    //controls = new THREE.OrbitControls(Camara, Render.domElement);
 }
 
-//agregando al escenario el punto o particula
-ParticulaMaterial = new THREE.ParticleBasicMaterial({color:0xFF0000});
-Particula = new THREE.ParticleSystem(Geometria, ParticulaMaterial);
-Escenario.add(Particula)
+function cargar_modelo() {
+    //Geometría para un punto rojo
+    Geometria = new THREE.Geometry()
+    ElVector = new THREE.Vector3(10, 0, 0);
+    Geometria.vertices.push(ElVector);
 
-//Agregando al escenario la figura
-Material = new THREE.ParticleBasicMaterial({color:0xFF0000});
-Figura = new THREE.Line(GeometriaLinea, Material);
-Escenario.add(Figura)
+    //Geometría para una figura
+    GeometriaLinea = new THREE.Geometry()
+    var vertices = [
+        [2, 7, 0],
+        [7, 2, 0],
+        [12, 7, 0],
+        [12, 17, 0],
+        [7, 12, 0],
+        [2, 17, 0],
+        [2, 7, 0],
+        [2, 7, 2],
+        [7, 2, 2],
+        [12, 7, 2],
+        [12, 17, 2],
+        [7, 12, 2],
+        [2, 17, 2],
+        [2, 7, 2]];
 
-Render.render(Escenario, Camara)
+    var long_vertices = vertices.length;
+
+    //Se analiza cada vector
+    for (i = 0; i < long_vertices; i++) {
+        x = vertices[i][0];
+        y = vertices[i][1];
+        z = vertices[i][2];
+        //Se agrega el vector a la geometría
+        Vector = new THREE.Vector3(x, y, z);
+        GeometriaLinea.vertices.push(Vector);
+    }
+
+    //agregando al escenario el punto o particula
+    ParticulaMaterial = new THREE.PointsMaterial({ color: 0xFF0000 });
+    Particula = new THREE.ParticleSystem(Geometria, ParticulaMaterial);
+    Escenario.add(Particula)
+
+    //Agregando al escenario la figura
+    Material = new THREE.PointsMaterial({ color: 0xFF0000 });
+    Figura = new THREE.Line(GeometriaLinea, Material);
+    Escenario.add(Figura)
+}
+
+function animacion() {
+    requestAnimationFrame(animacion);
+    render_modelo();
+}
+
+function render_modelo() {
+    //controls.update();
+    Figura.rotation.y += 0.01;
+    //Agregar el escenario y la cámara al render
+    Render.render(Escenario, Camara)
+}
